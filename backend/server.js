dc499d08dd033cfa829295c0e819cf6e81ec03c9
Akash Gupta <akashgupta7484@gmail.com>
2025-06-16 19:32:30 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,18 @@ const cors = require('cors');
 const contactRoutes = require('./routes/contactRoutes');
 
 const app = express();
-app.use(cors({
-  origin: true,        // allows all origins
-  credentials: true,   // if you want to support cookies or sessions
-}));
-app.use(express.json());
-app.use('/api/contact',contactRoutes);
 
-const PORT = process.env.PORT||5000;
-app.listen(PORT, ()=>{
-    console.log("Server started");
+app.use(cors({
+  origin: 'https://akportfolio2.vercel.app',
+  // credentials: true,
+}));
+
+// app.options('*', cors());
+
+app.use(express.json());
+app.use('/api/contact', contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server started");
 });
